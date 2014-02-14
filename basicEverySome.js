a279e8685ac9c77929a64191dc879f8970ceeb77
jmorille <jmorille@gmail.com>
2014-02-14 21:26:43 +0100

    Every Some passe

--- basicEverySome.js
+++ basicEverySome.js
@@ -3,5 +3,10 @@
 module.exports = function checkUsersValid(goodUsers) {
   return function(submittedUsers) {
     // SOLUTION GOES HERE
+    return submittedUsers.every(function(item) {    	
+    	return goodUsers.some(function(check){ 
+    		return item.id === check.id;
+    	});
+    });
   };
 }
---
+++ basicReduce.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function countWords(inputWords) {
+  // SOLUTION GOES HERE
+  console.log(inputWords);
+}
+
+module.exports = countWords
