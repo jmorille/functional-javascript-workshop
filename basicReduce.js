799cd88b915d6554500b65d74416c3c88ca4efc9
jmorille <jmorille@gmail.com>
2014-02-14 22:45:56 +0100

    Reduce

--- basicReduce.js
+++ basicReduce.js
@@ -1,8 +1,13 @@
 'use strict'
 
-function countWords(inputWords) {
-  // SOLUTION GOES HERE
-  console.log(inputWords);
+function countWords(inputWords) { 
+  // SOLUTION GOES HERE 
+  return inputWords
+  //.sort(function(a, b) { return a.localeCompare(b); })
+  .reduce( function(result, word) {  
+       result[word] = ++result[word] || 1;
+       return result;
+  }, {} ); 
 }
 
 module.exports = countWords
