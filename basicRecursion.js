411cd838c3eb28476edc914be2249eafb789d546
jmorille <jmorille@gmail.com>
2014-02-17 17:46:01 +0100

    Basic Recursion

--- basicRecursion.js
+++ basicRecursion.js
@@ -2,8 +2,17 @@
 'use strict'
 
 
-function duckCount() {
+function reduce(arr, fn, initial) {
     // SOLUTION GOES HERE
+    // if (arr.length<1) return initial;
+    //var previous = fn(arr.slice(1,1), initial);
+    //return reduce(arr.slice(1), fn, previous);
+    return (function reduceOne(index, value) {
+        if (index >= arr.length) return value;
+        reduceOne(index+1, fn(value, arr[index], index, arr));
+    })(0, initial);
+
 }
 
-module.exports = duckCount
+module.exports = reduce
+
