0db4b39ddae35e1df3692049dbb628e6ece7a76c
jmorille <jmorille@gmail.com>
2014-02-18 19:46:18 +0100

    tree

--- recursion.js
+++ recursion.js
@@ -9,6 +9,23 @@ function getDependencies(tree) {
     // Note: Feel free to add additional arguments
     // to this function for use with recursive calls.
     // Or not! There are many ways to recurse.
+
+    var result = [];
+
+    if (tree.hasOwnProperty("dependencies")) {
+        var node = tree["dependencies"];
+        Object.keys(node).forEach(function(key) {
+            var depName =  key + '@' + node[key]['version'];
+            result.push(depName);
+            result = result.concat(  getDependencies(node[key]) );
+        });
+    }
+    // sort result
+    var sorted = result.sort(function(a, b) {
+        return a.localeCompare(b);
+    });
+    return sorted;
+//    console.log('------ sorted',sorted, ' <== ', result);
 }
 
 module.exports = getDependencies
