8fea16a3bd7cc798d9ee7e44955b7a7515d1b7fb
jmorille <jmorille@gmail.com>
2014-02-18 17:58:24 +0100

    asyncLoops.js

--- asyncLoops.js
+++ asyncLoops.js
@@ -5,15 +5,25 @@
 
 function loadUsers(userIds, load, done) {
 
-    var users = userIds.map(function(userId) {
-        var userLoaded = {};
+    var users = [];
+    //users[3] = { 'bobo' : 'fff'};
+    var expectedCount = userIds.length;
+
+      userIds.map(function(userId, index) {
+        //console.log('userId', userId, 'index', index);
         load(userId, function(user) {
-            console.log('user', user);
-          //  userLoaded = user;
+            //console.log('userId', userId, 'user', user, 'index', index);
+            users[index] = user;
+            --expectedCount;
+      //     console.log('read missing',  expectedCount , ' ==> users', users);
         });
-        return userLoaded;
+        return {'index': index, 'userId': userId};
     });
 
+   // while ( expectedCount >0) {
+       // console.log('readCount', readCount, '/', expectedCount );
+   // }
+    console.log('result users', users );
     return users
 }
 
