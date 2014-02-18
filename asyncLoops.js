34dc2b797ef5c3825dc734307289523d5e6a56b6
jmorille <jmorille@gmail.com>
2014-02-18 16:10:44 +0100

    async loop TODO

---
+++ asyncLoops.js
@@ -0,0 +1,14 @@
+
+
+'use strict'
+
+
+function loadUsers(userIds, load, done) {
+    var users = [];
+    for (var i = 0; i < userIds.length; i++) {
+        users.push(load(userIds[i]));
+    }
+    return users
+}
+
+module.exports = loadUsers
