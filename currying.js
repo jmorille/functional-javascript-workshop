d5fceec3da2242998ce75b2433845eba16abc5ba
jmorille <jmorille@gmail.com>
2014-02-18 21:59:48 +0100

    currying

--- currying.js
+++ currying.js
@@ -3,6 +3,14 @@
 
 function curryN(fn, n) {
     // SOLUTION GOES HERE
+
+    n = n || arguments.length;
+    var args = Array.prototype.slice.call(arguments,0,n);
+    console.log('n', n, "==> args ", args, " for arguments", arguments);
+    return function() {
+        //console.log('  function', " args ", args, " for arguments", arguments, " ==> ", args.concat(Array.prototype.slice.call(arguments)));
+       return  fn.apply(fn, args.concat(Array.prototype.slice.call(arguments)));
+    };
 }
 
 module.exports = curryN
