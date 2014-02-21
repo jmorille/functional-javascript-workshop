64a7404044694e92b23afc4df33207e346a71717
jmorille <jmorille@gmail.com>
2014-02-21 23:37:41 +0100

    lines

--- stream-adventure/lines.js
+++ stream-adventure/lines.js
@@ -2,16 +2,19 @@ var through = require('through');
 var split = require('split');
 
 
-var count = 0;
+var upper = function () {
+    var count = 0;
+    return through(function write(buf) {
+        var line = buf.toString();
+        if (++count % 2 === 0) {
+            this.queue(line.toLocaleUpperCase());
+        } else {
+            this.queue(line.toLocaleLowerCase());
+        }
+        this.queue('\n');
+    });
+};
 
-var upper = through(function write(buf) {
-    var line = buf.toString();
-    if (++count % 2 === 0) {
-        this.queue(line.toLocaleUpperCase());
-    } else {
-        this.queue(line.toLocaleLowerCase());
-    }
-    this.queue('\n');
-});
-
-process.stdin.pipe(split()).pipe(upper).pipe(process.stdout);
+process.stdin.pipe(split())
+    .pipe(upper())
+    .pipe(process.stdout);
